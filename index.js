10801f274ca4510e2bd55a6da921a749cd5b8cf3
Bobby Naluz <bnaluz9@gmail.com>
2024-08-28 16:33:22 -0400

    fix: scraper + add email capability

--- index.js
+++ index.js
@@ -1,128 +1,171 @@
+//*FINAL !
+require('dotenv').config();
+const nodemailer = require('nodemailer');
 const { chromium } = require('playwright');
 const fs = require('fs');
 const { parse } = require('json2csv');
 
-const getREIPrices = async () => {
+const sendEmailWithAttachments = async (attachments) => {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: process.env.RECIPIENT_EMAIL,
+    subject: 'Scraped Data CSV Files',
+    text: 'Attached are the CSV files with the scraped data.',
+    attachments,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully!');
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+const scrapeDeals = async (url, fileName) => {
   const browser = await chromium.launch({
     headless: false,
   });
 
-  const context = await browser.newContext();
+  const headers = {
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0',
+    Accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    Connection: 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    Pragma: 'no-cache',
+    'Cache-Control': 'no-cache',
+  };
+
+  const context = await browser.newContext({
+    extraHTTPHeaders: headers,
+  });
 
   const page = await context.newPage();
 
   await page.goto('https://www.rei.com/');
 
   try {
-    await page
-      .locator('button[data-analytics-id="rei_nav:rei_topleveltab_deals"]')
-      .click();
-
-    await page
-      .locator(
-        "a[data-analytics-id='rei_nav:deals_footwear deals_men\\'s footwear']"
-      )
-      .click();
+    await page.goto(url, {
+      waitUntil: 'networkidle',
+      timeout: 60000,
+    });
 
     await page.waitForTimeout(Math.random() * 2000 + 1000);
-    await page.mouse.move(0, 200);
 
-    await page.locator('button[class="zCU31v49r7TwTyKHRBF2"]').click();
-    await page
-      .locator('input[id="products-search-zip"]')
-      .fill('Ann Arbor, MI', { delay: 100 });
+    const productData = await page.$$eval(
+      'ul.cdr-grid_13-5-2 > li.VcGDfKKy_dvNbxUqm29K',
+      (items) => {
+        return items.map((item) => {
+          const productUrl = item.querySelector('a')
+            ? 'rei.com' + item.querySelector('a').getAttribute('href')
+            : 'N/A';
+          const savingsPercent = item.querySelector(
+            'div[data-ui="savings-percent-variant2"]'
+          )
+            ? item
+                .querySelector('div[data-ui="savings-percent-variant2"]')
+                .textContent.trim()
+            : 'N/A';
+          const brand = item.querySelector('span[data-ui="product-brand"]')
+            ? item
+                .querySelector('span[data-ui="product-brand"]')
+                .textContent.trim()
+            : 'N/A';
+          const title = item.querySelector('span[data-ui="product-title"]')
+            ? item
+                .querySelector('span[data-ui="product-title"]')
+                .textContent.trim()
+            : 'N/A';
+          const rating = item.querySelector(
+            'span.cdr-rating__caption-sr_13-5-2'
+          )
+            ? item
+                .querySelector('span.cdr-rating__caption-sr_13-5-2')
+                .textContent.trim()
+            : 'N/A';
 
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(Math.random() * 2000 + 1000);
-    await page.mouse.move(0, 100);
-
-    await page.locator('a[aria-label="Select Ann Arbor"]').click();
-    await page.waitForTimeout(Math.random() * 2000 + 500);
-
-    await page
-      .locator('select[aria-label="Sort By"]')
-      .first()
-      .selectOption('percentageoff');
-
-    await page.waitForTimeout(Math.random() * 2000 + 5000);
-
-    //*This is currently working at Black Diesel Coffee which uses a public wifi - ran into issue when working at Drip Coffee which had a protected network
-
-    //TODO: Currently able to navigate to Ann Arbor REI's top deals [percentoff] for mens shoes. Need to grab result grid, and extract the product data, its presented as multiple list items, and there is a data-color-swatch attribute which has a value of 'product/<productNum>/' and an <a> tag which has the link to the product. Now need to get the desired values to then export into csv file
-
-    //*1 - Grab all the items
-
-    //*1 Grab the unordered list using its specific class
-    await page.waitForSelector('ul.cdr-grid_13-5-2');
-    const allListItems = page.locator(
-      'ul.cdr-grid_13-5-2 > li.VcGDfKKy_dvNbxUqm29K'
+          return { productUrl, savingsPercent, brand, title, rating };
+        });
+      }
     );
-
-    //* Get the count of all list items
-    const listItemCount = await allListItems.count();
-    console.log(listItemCount);
-    const productData = [];
-
-    // //*2 Iterate through each list item
-    for (let i = 0; i < listItemCount; i++) {
-      const item = allListItems.nth(i);
-      const product = {};
-
-      const linkElement = await item.locator('a').first();
-      if ((await linkElement.count()) > 0) {
-        product.productUrl =
-          'rei.com' + (await linkElement.getAttribute('href'));
-      } else {
-        console.log(`No link found for item ${i}`);
-      }
-
-      const savingsElement = await item.locator(
-        'div[data-ui="savings-percent-variant2"]'
-      );
-      if ((await savingsElement.count()) > 0) {
-        product.savingsPercent = await savingsElement.textContent();
-      } else {
-        console.log(`No savings element found for item ${i}`);
-      }
-
-      const brandElement = await item.locator('span[data-ui="product-brand"]');
-      if ((await brandElement.count()) > 0) {
-        product.brand = await brandElement.textContent();
-      } else {
-        console.log(`No brand element found for item ${i}`);
-      }
-
-      const titleElement = await item.locator('span[data-ui="product-title"]');
-      if ((await titleElement.count()) > 0) {
-        product.title = await titleElement.textContent();
-      } else {
-        console.log(`No title element found for item ${i}`);
-      }
-
-      const ratingElement = await item.locator(
-        'span.cdr-rating__caption-sr_13-5-2'
-      );
-      if ((await ratingElement.count()) > 0) {
-        product.rating = await ratingElement.textContent();
-      } else {
-        console.log(`No rating element found for item ${i}`);
-      }
-
-      productData.push(product);
-    }
 
     if (productData.length > 0) {
       const csv = parse(productData);
-      fs.writeFileSync('productData.csv', csv);
-      console.log('CSV file created successfully.');
+      fs.writeFileSync(`csv/${fileName}`, csv);
+      console.log(`CSV file "${fileName}" created successfully.`);
     } else {
       console.log('No data available to convert to CSV.');
     }
   } catch (e) {
-    console.log(e);
+    console.error('Error during operation:', e);
+  } finally {
+    await browser.close();
   }
 };
 
+const scrapeMultipleURLs = async () => {
+  const urls = [
+    {
+      url: 'https://www.rei.com/c/mens-footwear/f/scd-deals?sort=percentageoff&ir=category%3Amens-footwear%3Bdeals%3ASee+All+Deals&r=c%3Bf%3Bstores%3A124',
+      fileName: 'mensFootwear.csv',
+    },
+    {
+      url: 'https://www.rei.com/c/womens-footwear/f/scd-deals?ir=category%3Awomens-footwear%3Bdeals%3ASee+All+Deals&r=c%3Bf%3Bstores%3A124&sort=percentageoff',
+      fileName: 'womensFootwear.csv',
+    },
+    {
+      url: 'https://www.rei.com/c/womens-jackets/f/scd-deals?ir=category%3Awomens-jackets%3Bdeals%3ASee+All+Deals&r=c%3Bf%3Bstores%3A124&sort=percentageoff',
+      fileName: 'womensJackets.csv',
+    },
+    {
+      url: 'https://www.rei.com/c/mens-jackets/f/scd-deals?ir=category%3Amens-jackets%3Bdeals%3ASee+All+Deals&r=c%3Bf%3Bstores%3A124&sort=percentageoff',
+      fileName: 'mensJackets.csv',
+    },
+    {
+      url: 'https://www.rei.com/c/tents/f/scd-deals?ir=category%3Atents%3Bdeals%3ASee+All+Deals&r=c%3Bf%3Bstores%3A124&sort=percentageoff',
+      fileName: 'tents.csv',
+    },
+    {
+      url: 'https://www.rei.com/c/backpacks/f/scd-deals?ir=category%3Abackpacks%3Bdeals%3ASee+All+Deals&r=c%3Bf%3Bstores%3A124&sort=percentageoff',
+      fileName: 'backpacks.csv',
+    },
+  ];
+
+  const attachments = [];
+
+  for (const { url, fileName } of urls) {
+    await scrapeDeals(url, fileName);
+
+    attachments.push({
+      filename: fileName,
+      path: `./csv/${fileName}`,
+    });
+
+    const delay = Math.floor(Math.random() * 7000) + 5000;
+    console.log(
+      `Waiting for ${delay} milliseconds before processing the next URL...`
+    );
+    await new Promise((resolve) => setTimeout(resolve, delay));
+  }
+
+  await sendEmailWithAttachments(attachments);
+};
+
 (async () => {
-  await getREIPrices();
+  await scrapeMultipleURLs();
 })();
